93184a191bcfca6a5db9e90c31e860e1929da37d
SAZZAD1Q2 <kurulia115@gmail.com>
2023-10-10 19:55:22 +0600

    nav added

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+# app/models/user.rb
+class User < ApplicationRecord
+    # Define the "name" and "email" attributes
+    validates :name, presence: true, length: { maximum: 255 }
+    validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  
+    # Other attributes and associations can be defined here as well
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Add a route for the "users" page
+  get '/users', to: 'users#index', as: 'users'
+
+
+
 end
+
