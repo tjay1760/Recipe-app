e10f4db48f4fca3a6267e537efd291e4784cb015
tjay1760 <mcjthiongo@gmail.com>
2023-10-10 06:18:33 +0300

    reconfigure routes

---
+++ app/controllers/public_recipe_controller.rb
@@ -0,0 +1,6 @@
+class PublicRecipeController < ApplicationController
+  def index
+    # You can add any logic or instance variables here if needed.
+    # For now, it's just rendering the HTML template.
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'recipe_food/index'
-  get 'recipe_food/show'
-  get 'recipe/index'
-  get 'recipe/show'
-  get 'food/index'
-  get 'users/index'
+  root 'users#index'
+
+  resources :recipe, only: %i[index show]
+  resources :food, only: %i[index show]
+  resources :recipe_food, only: %i[index show]
+  resources :public_recipe, only: [:index]
+  resources :general_shopping_list, only: [:index]
 end
