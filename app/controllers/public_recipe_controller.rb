c1784fab0aa3026335f76b88737ca3e395a11b83
tjay1760 <mcjthiongo@gmail.com>
2023-10-10 23:04:29 +0300

    Add public recipes

--- app/controllers/public_recipe_controller.rb
+++ app/controllers/public_recipe_controller.rb
@@ -1,6 +1,5 @@
 class PublicRecipeController < ApplicationController
   def index
-    # You can add any logic or instance variables here if needed.
-    # For now, it's just rendering the HTML template.
+    @public_recipes = Recipe.where(public: true).includes(:user, :recipe_foods)
   end
 end
