12fe39b7d01d61db8e68814a2be58b9f5e557464
tjay1760 <mcjthiongo@gmail.com>
2023-10-10 20:19:42 +0300

    Fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,3 @@
-
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -8,4 +7,3 @@ class User < ApplicationRecord
   has_many :foods, foreign_key: :user_id
   has_many :recipes, foreign_key: :user_id
 end
-
