52e8bbcdca2ab1a45bef471060b8f3f682c7086b
tjay1760 <mcjthiongo@gmail.com>
2023-10-11 14:45:59 +0300

    Imliment changes for show recipe food

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_10_10_073948) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_11_100436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +31,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_10_073948) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "food_id"
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
     t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
     t.index ["user_id"], name: "index_recipe_foods_on_user_id"
   end
@@ -62,8 +63,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_10_073948) do
   end
 
   add_foreign_key "foods", "users"
+  add_foreign_key "recipe_foods", "foods"
   add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipe_foods", "users"
   add_foreign_key "recipes", "users"
-
 end
