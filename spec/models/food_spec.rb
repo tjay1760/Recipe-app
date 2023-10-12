60176190fb2618c4c75af53133f155f96690b99e
tjay1760 <mcjthiongo@gmail.com>
2023-10-12 22:45:43 +0300

    Add test for the models

---
+++ spec/models/food_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Food, type: :model do
+  user = User.new(name: 'John Doe')
+  food = Food.new(
+    user: user,
+    name: 'Delicious Dish',
+    measurement_unit: 'Ounce',
+    price: 12.99,
+    quantity: 5
+  )
+
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      expect(food).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      food.name = nil
+      expect(food).to_not be_valid
+    end
+
+    it 'is not valid without a measurement_unit' do
+      food.measurement_unit = nil
+      expect(food).to_not be_valid
+    end
+
+    it 'is not valid without a price' do
+      food.price = nil
+      expect(food).to_not be_valid
+    end
+
+    it 'is not valid without a quantity' do
+      food.quantity = nil
+      expect(food).to_not be_valid
+    end
+  end
+end
