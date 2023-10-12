02aa561d05eccf58fee683dd97989d677ca172a5
tjay1760 <mcjthiongo@gmail.com>
2023-10-12 19:14:02 +0300

    Impliment cascade delete

--- db/migrate/20231011100436_add_food_id_to_recipe_foods.rb
+++ db/migrate/20231011100436_add_food_id_to_recipe_foods.rb
@@ -1,5 +1,5 @@
 class AddFoodIdToRecipeFoods < ActiveRecord::Migration[7.0]
   def change
-    add_reference :recipe_foods, :food, foreign_key: true
+    add_reference :recipe_foods, :food, foreign_key: { on_delete: :cascade }
   end
 end
