3c03ff8938bc629add8bb886c3b133886f0cfb7a
tjay1760 <mcjthiongo@gmail.com>
2023-10-13 23:27:23 +0300

    Add general shoping list math

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,28 +1,20 @@
 class GeneralShoppingListController < ApplicationController
   def index
-    @user = current_user
-    @recipe = Recipe.find(params[:recipe_id])
+    recipe_id = params[:recipe_id]
+    return unless recipe_id.present?
 
-    @user_foods_ids = @user.foods.pluck(:id)
-    @recipe_foods_ids = @recipe.recipe_foods.pluck(:food_id)
-
-    # Calculate missing food ids
-    missing_food_ids = @recipe_foods_ids - @user_foods_ids
-
-    # Fetch missing food records in one query
-    @missing_food_records = Food.where(id: missing_food_ids)
-
-    @food_prices = {}
-    @sum = 0
-
-    # Calculate food prices and the total sum
-    @missing_food_records.each do |food|
-      recipe_food = @recipe.recipe_foods.find_by(food_id: food.id)
-      price = recipe_food ? food.price * recipe_food.quantity.to_i : 0
-      @food_prices[food.id] = price
-      @sum += price
-    end
-
+    @recipe = Recipe.find(recipe_id)
+    @foods = current_user.foods
     @recipe_foods = @recipe.recipe_foods
+    @items_to_buy = 0
+    @needed_money = 0
+
+    @recipe_foods.each do |recipe_food|
+      @food = Food.find(recipe_food.food_id)
+      if recipe_food.quantity < @food.quantity
+        @items_to_buy += @food.quantity - recipe_food.quantity
+        @needed_money += @food.price * (@food.quantity - recipe_food.quantity)
+      end
+    end
   end
 end
