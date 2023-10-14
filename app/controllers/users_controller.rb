760eff8108316db1bc2e3e5b556370bf4812ece4
tjay1760 <mcjthiongo@gmail.com>
2023-10-14 07:46:05 +0300

    Impliment requested changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,11 @@
-# app/controllers/users_controller.rb
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def index
-    # Retrieve a list of users (e.g., User.all) and assign it to an instance variable
     @users = User.all
   end
 
-  def show
-    # The "set_user" method (defined below) fetches the user by ID, and we assign it to an instance variable
-    # This action will display the details of a specific user
-  end
-
-  # Define other actions as needed
+  def show; end
 
   private
 
